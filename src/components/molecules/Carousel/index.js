80e1fd435e1b9a83aabb8191b9e498853479cafe
maliksaepul <malik@inspigo.id>
2021-07-09 15:00:15 +0700

    bismillah

--- src/components/molecules/Carousel/index.js
+++ src/components/molecules/Carousel/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import cx from 'classnames';
-import * as styles from './carousel.module.scss'
+import React from "react"
+import cx from "classnames"
+import * as styles from "./carousel.module.scss"
+import arrow_left from "src/images/icons/left.svg"
+import arrow_right from "src/images/icons/right.svg"
+import Illu from 'components/molecules/Illu'
 
-const Carousel = ({ children, className, title}) => (
-    <div>
-        <h3>{title}</h3>
-         <div className={cx(styles.carousel, className)}>
-        {children}
+const Carousel = ({ children, className, title }) => (
+  <div>
+    <div className={styles.header_container}>
+      <h3 className={styles.header_container__title}>{title}</h3>
+      <div className={styles.header_container__navigation}>
+       <Illu src={arrow_left} className={styles.header_container__navigation___item} />
+       <Illu src={arrow_right} className={styles.header_container__navigation___item} />
+      </div>
     </div>
-    </div>
-   
+    <div className={cx(styles.carousel, className)}>{children}</div>
+  </div>
 )
 
-export default Carousel;
+export default Carousel
