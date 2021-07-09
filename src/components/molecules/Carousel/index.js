6fac960ca1d6087a105027ed2c777f60d9579608
maliksaepul <malik@inspigo.id>
2021-07-09 15:26:12 +0700

    bismillah

--- src/components/molecules/Carousel/index.js
+++ src/components/molecules/Carousel/index.js
@@ -1,21 +1,37 @@
-import React from "react"
+import React,{useRef} from "react"
 import cx from "classnames"
 import * as styles from "./carousel.module.scss"
 import arrow_left from "src/images/icons/left.svg"
 import arrow_right from "src/images/icons/right.svg"
-import Illu from 'components/molecules/Illu'
 
-const Carousel = ({ children, className, title }) => (
-  <div>
-    <div className={styles.header_container}>
-      <h3 className={styles.header_container__title}>{title}</h3>
-      <div className={styles.header_container__navigation}>
-       <Illu src={arrow_left} className={styles.header_container__navigation___item} />
-       <Illu src={arrow_right} className={styles.header_container__navigation___item} />
+const Carousel = ({ children, className, title }) => {
+  const ref = useRef(null)
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
+
+  return (
+    <div>
+      <div className={styles.header_container}>
+        <h3 className={styles.header_container__title}>{title}</h3>
+        <div className={styles.header_container__navigation}>
+          <div
+            className={styles.header_container__navigation___item}
+            onClick={() => scroll(-600)}
+          >
+            <img src={arrow_left} alt="" />
+          </div>
+          <div
+            className={styles.header_container__navigation___item}
+            onClick={() => scroll(600)}
+          >
+            <img src={arrow_right} alt="" />
+          </div>
+        </div>
       </div>
+      <div className={cx(styles.carousel, className)} ref={ref} >{children}</div>
     </div>
-    <div className={cx(styles.carousel, className)}>{children}</div>
-  </div>
-)
+  )
+}
 
 export default Carousel
