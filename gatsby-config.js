7950a20595a1ec129a7a0d8eea442ffe6ddeb904
maliksaepul <malik@inspigo.id>
2021-07-21 17:19:25 +0700

    add analytic

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
           id: "GTM-TNCBPVL",
-          includeInDevelopment: true,
+          includeInDevelopment: false,
           routeChangeEventName: "route-change"
       }
     },
